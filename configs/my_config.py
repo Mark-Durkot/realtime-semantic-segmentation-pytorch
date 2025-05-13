0b48b7e9614b3dee3bf41d16f0dbb802be53e6cc
Mark Durkot <mark.durkot@gmail.com>
2025-05-13 19:25:36 +0200

    Update my_config.py

--- configs/my_config.py
+++ configs/my_config.py
@@ -13,7 +13,7 @@ class MyConfig(BaseConfig):
         self.num_class = 6
 
         # Model
-        self.model = 'ppliteseg'
+        self.model = 'stdc'
 
         # Training
         self.total_epoch = 1000
