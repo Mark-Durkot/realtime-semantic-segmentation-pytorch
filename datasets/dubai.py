f347f204f151ea7f2bbec8b483d5316e45b56098
Mark Durkot <mark.durkot@gmail.com>
2025-05-14 02:03:51 +0200

    Update dubai.py

--- datasets/dubai.py
+++ datasets/dubai.py
@@ -11,7 +11,7 @@ from patchify import patchify
 
 from utils import transforms
 from .dataset_registry import register_dataset
-
+from sklearn.preprocessing import MinMaxScaler
 
 @register_dataset
 class Dubai(Dataset):
@@ -65,7 +65,7 @@ class Dubai(Dataset):
 
         self.images = []
         self.masks = []
-
+        scaler = MinMaxScaler()
         # Process each tile folder
         for tile_num in range(1, 9):  # Tile 1 to Tile 8
             tile_folder = os.path.join(data_root, f'Tile {tile_num}')  # Note the space after 'Tile'
@@ -127,14 +127,85 @@ class Dubai(Dataset):
                 # Store patches
                 for i in range(patches_img.shape[0]):
                     for j in range(patches_img.shape[1]):
-                        img_patch = patches_img[i,j,:,:][0]
-                        mask_patch = patches_mask[i,j,:,:][0]
+                        img_patch = patches_img[i,j,:,:]
+                        mask_patch = patches_mask[i,j,:,:]
+
+                        img_patch = scaler.fit_transform(img_patch.reshape(-1, img_patch.shape[-1])).reshape(img_patch.shape)
                         
                         self.images.append(img_patch)
                         self.masks.append(mask_patch)
 
         if len(self.images) == 0:
             raise RuntimeError("No valid images found in the dataset")
+
+
+#     image_dataset = []  
+#     for path, subdirs, files in os.walk(root_directory):
+#         #print(path)  
+#         dirname = path.split(os.path.sep)[-1]
+#         if dirname == 'images':   #Find all 'images' directories
+#             images = os.listdir(path)  #List of all image names in this subdirectory
+#             for i, image_name in enumerate(images):  
+#                 if image_name.endswith(".jpg"):   #Only read jpg images...
+                
+#                     image = cv2.imread(path+"/"+image_name, 1)  #Read each image as BGR
+#                     SIZE_X = (image.shape[1]//patch_size)*patch_size #Nearest size divisible by our patch size
+#                     SIZE_Y = (image.shape[0]//patch_size)*patch_size #Nearest size divisible by our patch size
+#                     image = Image.fromarray(image)
+#                     image = image.crop((0 ,0, SIZE_X, SIZE_Y))  #Crop from top left corner
+#                     #image = image.resize((SIZE_X, SIZE_Y))  #Try not to resize for semantic segmentation
+#                     image = np.array(image)             
+        
+#                     #Extract patches from each image
+#                     print("Now patchifying image:", path+"/"+image_name)
+#                     patches_img = patchify(image, (patch_size, patch_size, 3), step=patch_size)  #Step=256 for 256 patches means no overlap
+            
+#                     for i in range(patches_img.shape[0]):
+#                         for j in range(patches_img.shape[1]):
+                            
+#                             single_patch_img = patches_img[i,j,:,:]
+                            
+#                             #Use minmaxscaler instead of just dividing by 255. 
+#                             single_patch_img = scaler.fit_transform(single_patch_img.reshape(-1, single_patch_img.shape[-1])).reshape(single_patch_img.shape)
+                            
+#                             #single_patch_img = (single_patch_img.astype('float32')) / 255. 
+#                             single_patch_img = single_patch_img[0] #Drop the extra unecessary dimension that patchify adds.                               
+#                             image_dataset.append(single_patch_img)
+                
+  
+                
+  
+#  #Now do the same as above for masks
+#  #For this specific dataset we could have added masks to the above code as masks have extension png
+
+#     for path, subdirs, files in os.walk(root_directory):
+#         #print(path)  
+#         dirname = path.split(os.path.sep)[-1]
+#         if dirname == 'masks':   #Find all 'images' directories
+#             masks = os.listdir(path)  #List of all image names in this subdirectory
+#             for i, mask_name in enumerate(masks):  
+#                 if mask_name.endswith(".png"):   #Only read png images... (masks in this dataset)
+                
+#                     mask = cv2.imread(path+"/"+mask_name, 1)  #Read each image as Grey (or color but remember to map each color to an integer)
+#                     mask = cv2.cvtColor(mask,cv2.COLOR_BGR2RGB)
+#                     SIZE_X = (mask.shape[1]//patch_size)*patch_size #Nearest size divisible by our patch size
+#                     SIZE_Y = (mask.shape[0]//patch_size)*patch_size #Nearest size divisible by our patch size
+#                     mask = Image.fromarray(mask)
+#                     mask = mask.crop((0 ,0, SIZE_X, SIZE_Y))  #Crop from top left corner
+#                     #mask = mask.resize((SIZE_X, SIZE_Y))  #Try not to resize for semantic segmentation
+#                     mask = np.array(mask)             
+        
+#                     #Extract patches from each image
+#                     print("Now patchifying mask:", path+"/"+mask_name)
+#                     patches_mask = patchify(mask, (patch_size, patch_size, 3), step=patch_size)  #Step=256 for 256 patches means no overlap
+            
+#                     for i in range(patches_mask.shape[0]):
+#                         for j in range(patches_mask.shape[1]):
+                            
+#                             single_patch_mask = patches_mask[i,j,:,:]
+#                             #single_patch_img = (single_patch_img.astype('float32')) / 255. #No need to scale masks, but you can do it if you want
+#                             single_patch_mask = single_patch_mask[0] #Drop the extra unecessary dimension that patchify adds.                               
+#                             mask_dataset.append(single_patch_mask)
 
     def __len__(self):
         return len(self.images)
