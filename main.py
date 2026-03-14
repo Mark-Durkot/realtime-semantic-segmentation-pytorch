72a7dfe7b8e2da0a1cbc9c55d15e139c7a2a38d3
Mark Durkot <mark.durkot@gmail.com>
2026-03-14 13:24:31 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from core import SegTrainer
-from configs import VDDConfig, MyConfig, load_parser
+from configs import VDDConfig, MyConfig, load_parser, OptunaConfig
 
 import warnings
 warnings.filterwarnings("ignore")
 
 
 if __name__ == '__main__':
-    config = VDDConfig()
+    config = OptunaConfig()
 
     config.init_dependent_config()
 
